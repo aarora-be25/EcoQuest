43235fcc5fd41ac956beff90cb3bd0517e42daf0
Apoorva Arora <aarora4_be25@thapar.edu>
2026-04-11 20:10:28 +0530

    Update ProfileScreen.js

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -143,7 +143,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between',
     backgroundColor: COLORS.primary, padding: SPACING.base,
   },
-  backText: { fontSize: 14, fontWeight: FONTS.semibold, color: COLORS.white },
+  backText: { paddingTop: SPACING.xxl + SPACING.md, fontSize: 14, fontWeight: FONTS.semibold, color: COLORS.white },
   headerTitle: { fontSize: 17, fontWeight: FONTS.heavy, color: COLORS.white },
 
   body: { flex: 1, padding: SPACING.base },
